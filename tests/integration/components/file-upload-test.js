53983e6fea9785750b798f11dd91b6e65bd5f628
Joel Kuntz <joel.kuntz.92@gmail.com>
2019-03-30 12:44:31 -0300

    file-upload tests

--- tests/integration/components/file-upload-test.js
+++ tests/integration/components/file-upload-test.js
@@ -1,26 +1,51 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | file-upload', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders with no parameters', async function(assert) {
+
+    /*
+      <FileUpload
+        @change={{this.importData}}
+        accept=".json"
+      />
+    */
 
     await render(hbs`<FileUpload />`);
+    let inputDom = assert.dom('input');
+    inputDom.hasAttribute('type', 'file');
+    inputDom.doesNotHaveAttribute('accept');
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('it renders with accept', async function(assert) {
     await render(hbs`
-      <FileUpload>
-        template block text
-      </FileUpload>
+      <FileUpload
+        accept=".json"
+      />
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    let inputDom = assert.dom('input');
+    inputDom.hasAttribute('type', 'file');
+    inputDom.hasAttribute('accept', '.json');
+  });
+
+  test('it handles upload', async function(assert) {
+    assert.expect(1);
+
+    this.set('onchange', (_name, result) => {
+      assert.equal(result, 'My text based file');
+    });
+
+    await render(hbs`
+      <FileUpload
+        @change={{this.onchange}}
+      />
+    `);
+
+    await triggerEvent('input', 'change', [ new Blob(['My text based file']) ]);
   });
 });
