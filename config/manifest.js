60a16b06f01e14b32d49ad705bdda1636d25e6bf
Joel Kuntz <joel.kuntz.92@gmail.com>
2019-04-07 23:06:41 -0300

    #6 install service worker and webapp packages

---
+++ config/manifest.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/zonkyio/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "Track Your Goals",
+    short_name: "Track Your Goals",
+    description: "An app for tracking daily goals",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
