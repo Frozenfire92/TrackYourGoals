35a07eafdea8ab612fd6995627cf00fa9b21aa63
Joel Kuntz <joel.kuntz.92@gmail.com>
2019-04-20 01:04:49 -0300

    #2 clean up dark mode ensurance

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
+  @service settings
+
+  constructor(){
+    super(...arguments);
+    // Ensure we set this initially or dark mode won't show up until called
+    this.settings.appColorScheme;
+  }
+
   redirect(model, transition){
     if (transition.targetName === 'index') {
       this.transitionTo('goals');
