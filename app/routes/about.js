22d14dcc2aea07230fb02ed261eb0ac59735dac3
Joel Kuntz <joel.kuntz.92@gmail.com>
2019-04-20 01:08:43 -0300

    show version on about page

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import ENV from 'track-your-goals/config/environment';
 
-export default class AboutRoute extends Route {}
+export default class AboutRoute extends Route {
+  model() {
+    return {
+      version: ENV.APP.version
+    }
+  }
+}
