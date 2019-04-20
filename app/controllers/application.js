5d2b744722f013e14788d308bd142a55e70ffe92
Joel Kuntz <joel.kuntz.92@gmail.com>
2019-04-20 00:56:47 -0300

    trying to fix mobile dark glitch

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,15 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
+  constructor(){
+    super(...arguments);
+    console.log(this.settings.appColorScheme);
+  }
+
+  @service settings
+
   @computed('target.currentPath')
   get pathClass() {
     return this.target.currentPath.replace(/\./g, '-');
