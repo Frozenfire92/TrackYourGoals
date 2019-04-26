8e4fa3246f466be484e0c0aba13f5d49293e032d
Joel Kuntz <joel.kuntz.92@gmail.com>
2019-04-25 21:57:51 -0300

    call appColorScheme in ApplicationRoute

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,11 @@ import { inject as service } from '@ember/service';
 export default class ApplicationRoute extends Route {
   @service settings
 
+  beforeModel(){
+    // Call this initially to set the class on the html element
+    this.settings.appColorScheme;
+  }
+
   redirect(model, transition){
     if (transition.targetName === 'index') {
       this.transitionTo('goals');
--- app/services/settings.js
+++ app/services/settings.js
@@ -4,12 +4,6 @@ import { inLocalStorage, setHtmlClass } from 'track-your-goals/utils/decorators'
 import visualizationColorSchemes from 'track-your-goals/utils/visualization-color-schemes';
 
 export default class SettingsService extends Service {
-  constructor(){
-    super(...arguments);
-    // Call this initially to set the class on the html element
-    this.appColorScheme;
-  }
-
   @setHtmlClass
   @inLocalStorage
   appColorScheme = 'light';
